d532bc3e492e8160ff061befec47d9df1c451f26
Milovidov Vasya <milovidov.vasya@gmail.com>
2019-12-22 22:07:04 +0300

    pin class

--- src/BotKeeper.Service/Domain/Auth/AuthPin.cs
+++ src/BotKeeper.Service/Domain/Auth/AuthPin.cs
@@ -2,30 +2,49 @@
 using BotKeeper.Service.Domain.Store;
 using System;
 using System.Collections.Generic;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BotKeeper.Service.Domain.Auth {
 	public class AuthPin {
-		private readonly Dictionary<long, int> pinsCache = new Dictionary<long, int>();
 		private readonly IStore store;
 
 		public AuthPin(IStore store) {
 			this.store = store;
 		}
 
+		public int MAX_PIN_ATTEMPTS { get; private set; }
+
 		public async Task<bool> Validate(int inputPin, IUser user) {
-			var isCached = pinsCache.TryGetValue(user.Id, out var pin);
+			await CheckArguments(inputPin, user);
+			await CheckBusinessRules(user);
 
-			if (!isCached) {
-				pin = await store.GetPin(user);
-				pinsCache.TryAdd(user.Id, pin);
-			}
-
-			if(inputPin == pin) {
+			var pin = await store.GetPin(user);
+			if (inputPin == pin) {
+				await user.ResetPinAttempt();
 				return true;
 			}
+			await user.AddAttempt();
 			return false;
+		}
+
+		private async Task CheckBusinessRules(IUser user) {
+			int attempts = await user.GetPinAttempts();
+			if (attempts >= MAX_PIN_ATTEMPTS) {
+				await user.LockPin();
+				throw new SecurityException($"Maximum number of pin attempts was reached.");
+			}
+		}
+
+		private async Task CheckArguments(int inputPin, IUser user) {
+			if (inputPin == default) {
+				throw new ArgumentException($"{nameof(inputPin)} can not be default value.");
+			}
+			if (user is null) {
+				throw new ArgumentNullException($"{nameof(user)} can not be null.");
+			}
+
 		}
 	}
 }
--- src/BotKeeper.Service/Domain/Models/IUser.cs
+++ src/BotKeeper.Service/Domain/Models/IUser.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace BotKeeper.Service.Domain.Models {
 	public interface IUser {
 		long Id { get; }
+
+		Task ResetPinAttempt();
+		Task AddAttempt();
+		Task<int> GetPinAttempts();
+		Task LockPin();
 	}
 }
