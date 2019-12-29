6fb735eb3d16526d0d98dae15c8930a85ee852c9
Milovidov Vasya <milovidov.vasya@gmail.com>
2019-12-29 17:46:48 +0300

    storage

---
+++ src/BotKeeper.Service/Domain/Interfaces/IStorage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotKeeper.Service.Domain.Interfaces {
+	public interface IStorage {
+		Task Save<T>(int userId, string key, T value);
+	}
+}
---
+++ src/BotKeeper.Service/Persistence/Db/Storage.cs
@@ -0,0 +1,25 @@
+﻿using BotKeeper.Service.Domain.Interfaces;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+namespace BotKeeper.Service.Persistence.Db {
+	public class Storage : IStorage {
+		private ConcurrentDictionary<int, ConcurrentDictionary<string, string>> storage = 
+			new ConcurrentDictionary<int, ConcurrentDictionary<string, string>>();
+		public async Task Save<T>(int userId, string key, T value) {
+			await Task.Yield();
+			if (storage.TryGetValue(userId, out var userStorage)) {
+				var json = JsonSerializer.Serialize<T>(value);
+				var saveStatusOk = userStorage.TryAdd(key, json);
+				if(saveStatusOk) {
+					return;
+				} else {
+					userStorage[key] = json;
+				}
+			}
+		}
+	}
+}
